e89f458e67a2c8153cc37584e7b65015225a37ea
GABRIEL OLIVEIRA GUSMÃO FLORENCIO DOS SANTOS <gabriel.santos999@senaisp.edu.br>
2026-09-04 09:03:00 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = "Gabriel Oliveira Gusmão Florencio dos Santos";
+$profissao = "Aprendiz de engenharia de processos";
+$mensagem = "batatinha frita";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula git</title>
+</head>
+<body>
+    <h1><?php echo $nome ?></h1>
+
+    <h2><?php echo $profissao ?></h2>
+
+    <p><?php echo $mensagem ?></p>
+</body>
+</html>
